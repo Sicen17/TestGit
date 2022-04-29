7e4fa52c9ef2681c279bc8f625cbb1079d4f9cbe
Sicen <krystof.troler@gmail.com>
2022-04-29 10:45:09 +0200

    Project Structure #0

--- src/Random_Number.java
+++ src/Random_Number.java
@@ -1,24 +1,44 @@
 import java.util.Random;
 public class Random_Number
 {
-    private int Range_Number;
-    Random Rand = new Random(Range_Number+1);
+    Random Rand = new Random();
+    private int Range_Number_Max;
+    private int Range_Number_Min;
 
 
-    public Random_Number(int Range_Number)
+    public Random_Number()
     {
-        this.Range_Number = Range_Number;
+    }
+    public Random_Number(int Range_Number_Max, int Range_Number_Min)
+    {
+        this.Range_Number_Max = Range_Number_Max + 1;
+        this.Range_Number_Min = Range_Number_Min;
     }
 
 
-    public int getRange_Number()
+    public int Generate_Random()
     {
-        return Range_Number;
+        return Rand.nextInt(Range_Number_Max-Range_Number_Min) + Range_Number_Min;
     }
 
 
-    public void setRange_Number(int range_Number)
+    public int getRange_Number_Max()
     {
-        Range_Number = range_Number;
+        return Range_Number_Max;
+    }
+    public int getRange_Number_Min()
+    {
+        return Range_Number_Min;
+    }
+
+
+    public void setRange_Number_Max(int Range_Number_Max)
+    {
+        this.Range_Number_Max = Range_Number_Max;
+    }
+
+    public void setRange_Number_Min(int range_Number_Min)
+    {
+        Range_Number_Min = range_Number_Min;
     }
 }
