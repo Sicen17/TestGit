28080cf31ae4704ac9c10b459a359257a46e7986
Sicen <krystof.troler@gmail.com>
2022-04-29 09:58:24 +0200

    Projekt Struktura #0

--- src/Random_Number.java
+++ src/Random_Number.java
@@ -1,11 +1,10 @@
 import java.util.Random;
 public class Random_Number
 {
-    private int Range_Number_0;
-    private int Range_Number_1;
+    private int Range_Number;
+    Random Rand = new Random(Range_Number+1);
 
-    public Random_Number(int Range_Number_0, int Range_Number_1) {
-        this.Range_Number_0 = Range_Number_0;
-        this.Range_Number_1 = Range_Number_1;
+    public Random_Number(int Range_Number) {
+        this.Range_Number = Range_Number;
     }
 }
