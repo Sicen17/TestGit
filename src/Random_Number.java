3e3861fc100b1c62dc8f82c4586ae5359f2e43db
Sicen <krystof.troler@gmail.com>
2022-04-29 09:59:42 +0200

    Projekt Struktura #0

--- src/Random_Number.java
+++ src/Random_Number.java
@@ -4,7 +4,21 @@ public class Random_Number
     private int Range_Number;
     Random Rand = new Random(Range_Number+1);
 
-    public Random_Number(int Range_Number) {
+
+    public Random_Number(int Range_Number)
+    {
         this.Range_Number = Range_Number;
+    }
+
+
+    public int getRange_Number()
+    {
+        return Range_Number;
+    }
+
+
+    public void setRange_Number(int range_Number)
+    {
+        Range_Number = range_Number;
     }
 }
